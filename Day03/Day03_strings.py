ffdb2dfca97c03c388de880260c019ac623c5a5d
Luk Imperinetti <lukimperinetti@pop-os.localdomain>
2023-09-13 09:19:32 +0200

    Day03 -- Strings pdf ok

---
+++ Day03/Day03_strings.py
@@ -0,0 +1,23 @@
+# -----Strings----- #
+
+def task00():
+    word = "Voici une string"
+    print(word)
+
+def task01():
+    word = "Voici une string"
+    print(f'Premier : {word[0]} 5eme : {word[4]}')
+
+def task02():
+    word = "Voici une string"
+    print(f'last char : {word[-1]}')
+
+def task03():
+    word = "Voici une string"
+    print(f'Du 5eme au 10eme char : {word[5:11]}')
+
+
+# task00()
+# task01()
+# task02()
+task03()
