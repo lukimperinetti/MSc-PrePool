4331069f3df4c407bd4bd7096707d678691d8ef5
luk <luk.imperinetti@gmail.com>
2023-09-14 13:20:32 +0200

    challenge ok

---
+++ Day04/Day04_loops.py
@@ -0,0 +1,83 @@
+# ----- Loops ----- #
+
+def task00():
+    int = 0
+    while int <= 1000:
+        print(int)
+        int += 1
+
+def task01():
+    string = input('Entez une string >>> ')
+    double_string = ''
+    for letters in string:
+        double_string += letters*2
+    print(double_string)
+
+def task02():
+    start = 10000
+    end = 1
+
+    for cnt in range(start, end, -1):
+        if( cnt%7==0 ):
+            print(cnt)
+
+def task03():
+    minus = -30
+    max = 30
+    for cnt in range(minus, max):
+        if (cnt%3)==0:
+            print('Fizz')
+        elif (cnt%5)==0:
+            print('Buzz')
+        elif (cnt%3)==0 and (cnt%5)==0:
+            print('FizzBuzz')
+        else :
+            print(cnt)
+
+def task04():
+    lyrics = "99 bottles of age appropriate bottle on the wall"
+    s = ''.join(x for x in lyrics if x.isdigit())
+    converted = int(s)
+    while converted >= 0:
+        print(f'{converted} bottles of age appropriate bottle on the wall')
+        converted -= 1
+    
+
+def task05():
+    integer = int(input('Entrez un INT >>> '))
+    end = integer//2
+    start = 2
+
+    for cnt in range(start, end+1):
+        # if (integer%cnt)==0:
+        for num in range(start, integer+1):
+            if (integer%start)==0 :
+                print(num)
+            cnt += 1
+            
+        print('out')
+        start += 1
+
+
+def challenge():
+    integer = int(input('entrez un int >>> '))
+    string = input('Entrez une string')
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+
+    if integer == 0 : 
+        return 0
+    elif any(char in vowels for char in string):
+        print(integer)
+    elif integer >= 42:
+        print(integer)
+    else:
+        print(string)
+
+
+# task00()
+# task01()
+# task02()
+# task03()
+# task04()
+# task05() TODO : a faire
+challenge()
