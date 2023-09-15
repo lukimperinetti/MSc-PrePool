34643b17aa01701a15c337b042690da3cb66fad2
luk <luk.imperinetti@gmail.com>
2023-09-15 09:49:38 +0200

    Day05 loops

---
+++ Day05/Day05_list.py
@@ -0,0 +1,27 @@
+# ----- Lists creation and browsing ----- #
+
+def task01():
+    numbers = [1, 2, 3, 4, 5]
+    print(numbers[0])
+
+
+def task02():
+    numbers = [1, 2, 3, 4, 5, 6, 7]
+    print(numbers[-1])
+
+def task03():
+    numbers = [1, 2, 3, 4, 5, 6]
+    numbers.append(7)
+    print(numbers)
+
+def task04():
+    numbers = [1, 2, 3, 4, 5, 6, 7]
+    print(numbers)
+
+def task05():
+
+
+# task01()
+# task02()
+# task03()
+task04()
