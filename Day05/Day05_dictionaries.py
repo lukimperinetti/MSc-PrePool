9017d68ed0fac4ba77326139aa84580901e530bb
luk <luk.imperinetti@gmail.com>
2023-09-15 17:18:53 +0200

    task04 ok

--- Day05/Day05_dictionaries.py
+++ Day05/Day05_dictionaries.py
@@ -1,5 +1,9 @@
 # ----- Dictionaries ----- #
 
+import random
+import string
+
+
 def task01():
     students = {'name':'thomas', 'academic_year':2023}
     print(students)
@@ -74,9 +78,76 @@ def task03():
     # print(students)
 
 
+def task04():
+    
+    students = [
+        {
+            'name':'Thomas',
+            'academic_year':2023
+        },
+        {
+            'name':'Paul',
+            'academic_year':2023
+        },
+        {
+            'name':'Camille',
+            'academic_year':2023
+        }
+    ]
+   
+    grade_weight_mapping = {
+        "A":4,
+        "B":3,
+        "C":2,
+        "D":1,
+        "E":0
+    }
 
+    for i in students:
+        i['unit'] =  [
+            {
+            'name':'Network and System Administration',
+            'credits':random.randint(0,40),
+            'grade':chr(random.randint(ord('A'), ord('E')))
+            },
+            {
+            'name':'web development',
+            'credits':random.randint(0,40),
+            'grade':chr(random.randint(ord('A'), ord('E')))
+            },
+            {
+            'name':'java',
+            'credits':random.randint(0,40),
+            'grade':chr(random.randint(ord('A'), ord('E')))
+            }
+        ]
 
+    for index, value in enumerate(students):
+        total_credit = 0
+    
+        gpa_list = []
+        for units in value.get('unit'):
+            total_credit += units.get('credits') 
+            credit = units.get('credits') # me sort les crédits
+            grade = units.get('grade') # me sort les grade
+            for ele in grade_weight_mapping.keys():
+                if grade == ele:
+                    gpa_list.append(grade_weight_mapping.get(ele)*credit)
+        gpa = sum(gpa_list) / total_credit
+        value['GPA'] = round(gpa)
+        value['total_credits'] = total_credit #addition des crédit de unit
+
+    # tri par Name
+    # sorted_students = sorted(students, key=lambda x: x['name'])
+    # print(sorted_students) 
+
+    # tri par GPA
+    sorted_students = sorted(students, key=lambda x: x['GPA'])
+    print(sorted_students) 
+ 
+   
 
 # task01()
 # task02()
-task03()
+# task03()
+task04()
