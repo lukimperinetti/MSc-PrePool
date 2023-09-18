1cb3fe7a27fd53d1a12599e0d605b5f49f8ba5fb
luk <luk.imperinetti@gmail.com>
2023-09-18 14:33:55 +0200

    TAsk 00

--- Day06/Day06_higher-order-functions.py
+++ Day06/Day06_higher-order-functions.py
@@ -1,32 +1,69 @@
 # ----- Higher-order functions ----- #
 
+import re
+
+
 def task00():
-    
+
     def func1(string, number):
         print("Function 1")
         count=0
         for i in string:
-            if(i.islower()):
-                count=count+1
+            if i.islower():
+                count += 1
                 if count >= number:
                     return True
-                else:
-                    return False
+        return False
+
     is_lower = func1(string, number)
     print(is_lower)
 
 
     def func2(string, number):
-        print("World")
+        print("Function 2")
+        count=0
+        for i in string:
+            if i.isupper():
+                count += 1
+                if count >= number:
+                    return True
+        return False
+    is_upper = func2(string, number)
+    print(is_upper)
 
     def func3(string, number):
-        print("!")
+        print("Function 3")
+        count=1
+        for i in string:
+            if count >= number:
+                return True
+            count += 1
+        return False
+    counted = func3(string, number)
+    print(counted)
 
     def func4(string, number):
-        print("Bye")
+        print("Function 4")
+        count=1
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        for i in string:    
+            if count >= number and regex.search(string) == None:
+                return True
+            count += 1
+        return False
+    special_char = func4(string, number)
+    print(special_char)
 
     def func5(string, number):
-        print("World")
+        print("Function 5")
+        count=1
+        for i in string:
+            if i.isdigit() and count >= number  :
+                return True
+            count += 1
+        return False
+    digit = func5(string, number)
+    print(digit)
 
 
 string = input('entrez une chaine de caractères: ')
