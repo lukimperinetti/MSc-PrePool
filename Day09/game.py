a01d0c909db2608bef59038a2956779de8cabb57
luk <luk.imperinetti@gmail.com>
2023-09-21 14:47:02 +0200

    handle keyboard input

--- Day09/game.py
+++ Day09/game.py
@@ -2,6 +2,7 @@ import pygame
 import pytmx
 import pyscroll
 from player import Player
+import sys
 
 pygame.init()
 
@@ -22,21 +23,45 @@ class Game:
         player_position = tmx_data.get_object_by_name("player")
         self.player = Player(player_position.x, player_position.y)
 
-
         # draw the map
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=5)
         self.group.add(self.player)
 
+
+
+    def handle_input(self, key):
+        # Handle events
+        if key == pygame.K_ESCAPE:
+            pygame.quit()
+            quit()
+        else:
+            self.process_input(key)
+
+    def process_input(self, key):
+        # Handle the input
+        print("Key pressed:", key)
+        
+
     
     def run(self):
         # Game loop
-        runing = True
-        while runing:
+        running = True
+        while running:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    self.handle_input(event.key)
+
+            # Update the game state
             self.group.update()
+
+            # Draw the game
             self.group.draw(self.screen)
             pygame.display.flip()
 
-            for event in pygame.event.get(): # Get all the events
-                if event.type == pygame.QUIT: # If the event is quit
-                    runing = False
+
+
+
         pygame.quit() # Quit the game
--- Day09/player.py
+++ Day09/player.py
@@ -5,7 +5,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load("Day09/man.png")
+        self.sprite_sheet = pygame.image.load("/home/lukimperinetti/Documents/MSc-PrePool/Day09/man.png")
         self.image = self.get_image(0, 0)
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
